b58cfa2e893895dba3dff77ef59c6109b330d7ca
CarlosCastrillonM <carlangas.c.m.4@gmail.com>
2024-09-08 16:42:22 -0500

    BD connection

--- src/main/java/org/rsc/Main.java
+++ src/main/java/org/rsc/Main.java
@@ -1,11 +1,37 @@
 package org.rsc;
+
 import gui.*;
+import gui.FormOferta;
 
 
 public class Main {
     public static void main(String[] args) {
 
-        FormOferta fo = new FormOferta();
-        fo.setVisible(true);
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(FormOferta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(FormOferta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(FormOferta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(FormOferta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                //new FormOferta().setVisible(true);
+                FormOferta fo = new FormOferta();
+                fo.setVisible(true);
+            }
+        });
     }
 }
---
+++ src/main/java/util/RSC.java
@@ -0,0 +1,40 @@
+package util;
+
+import java.sql.*;
+import java.util.Properties;
+import gui.FormOferta;
+import org.rsc.Main;
+
+public class RSC {
+
+    private static final String GCP_URL = "35.199.121.103";
+    private static final String GCP_USER = "proyectopoo";
+    private static final String GCP_PASS = "alksjhdasdlkj";
+
+    public static void main(String[] args) throws SQLException {
+        Main.main(args);
+        if (true) {
+            return;
+        }
+
+        String url = "jdbc:postgresql://%s:5432/postgres".formatted(GCP_URL);
+        Properties props = new Properties();
+        props.setProperty("user", GCP_USER);
+        props.setProperty("password", GCP_PASS);
+        props.setProperty("ssl", "false");
+        Connection conn = DriverManager.getConnection(url, props);
+
+        Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery("SELECT 1");
+        rs.next();
+        rs.close();
+//
+//        while (rs.next()) {
+//            System.out.print("Column 1 returned ");
+//            System.out.println(rs.getString(3));
+//        }
+//        rs.close();
+//        st.close();
+        conn.close();
+    }
+}
